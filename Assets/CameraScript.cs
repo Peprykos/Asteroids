8b8c11336ada5f05c38af23aa4e64902c35dfa71
Patryk Hanna <patryk.hanna@teb.edu.pl>
2023-02-10 15:34:24 +0100

    Dodaj pliki projektów.

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float gameHeight;
+    public float gameWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float frustumHeight = 2.0f * Camera.main.transform.position.y
+                                * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        gameHeight= frustumHeight;
+        gameWidth = gameHeight * Camera.main.aspect;
+
+        Debug.Log("Wysokoœæ pola gry: " + gameHeight + ", szerokoœæ: " + gameWidth);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    //wartosc wczytana z klawiatury
+    Vector2 input;
+
+    //mnoznik przyspieszenia statku
+    public float enginePower = 10;
+
+    //mnoznik sterowania
+    public float gyroPower = 2;
+
+    private CameraScript cs;
+
+    //miejsce na prefab pocisku
+    public GameObject bulletPrefab;
+    //okresl miejsca spawnowania pociskow
+    public Transform GunLeft, GunRight;
+
+    //predkosc poczatkowa pocisku
+    public float bulletSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cs = Camera.main.transform.GetComponent<CameraScript>();
+        input = Vector2.zero;
+        GunLeft = transform.Find("GunLeft").transform;
+        GunRight = transform.Find("GunRight").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sterowanie w poziomie(a,d)
+        float x = Input.GetAxis("Horizontal");
+        //sterowanie w pionie(w,s)
+        float y = Input.GetAxis("Vertical");
+
+        input.x = x;
+        input.y = y;
+
+        //teleportuj statek jesli wyjdzie z ekranu
+        if (Mathf.Abs(transform.position.x) > cs.gameWidth / 2)
+        {
+            Vector3 newPosition = new Vector3(transform.position.x * (-0.99f),
+                                                0,
+                                                transform.position.z);
+            transform.position = newPosition;
+        }
+
+        if (Mathf.Abs(transform.position.z) > cs.gameHeight / 2)
+        {
+            Vector3 newPosition = new Vector3(transform.position.x,
+                                                0,
+                                                transform.position.z * (-0.99f));
+            transform.position = newPosition;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            Fire();
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * input.y * enginePower, ForceMode.Acceleration);
+        rb.AddTorque(transform.up * input.x * gyroPower, ForceMode.Acceleration);
+    }
+
+    void Fire()
+    {
+        GameObject leftBullet = Instantiate(bulletPrefab, GunLeft.position, Quaternion.identity);
+        //zmiana predkosci pocisku
+        leftBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed,
+                                                        ForceMode.VelocityChange);
+
+        //zniszcz pocisk po 5s
+        Destroy(leftBullet, 5);
+    }
+}
