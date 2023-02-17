a64f4b32d7e6a98abe8b71e224fc5eccedca200c
Patryk Hanna <patryk.hanna@teb.edu.pl>
2023-02-17 15:21:48 +0100

    asteroida i strzaly

---
+++ Assets/AsteroidScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        Vector3 movementVector = player.transform.position - transform.position;
+        rb.AddForce(movementVector, ForceMode.VelocityChange);
+        rb.AddTorque(new Vector3(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90)));
+        Destroy(gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LevelManager.cs
@@ -0,0 +1,75 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    CameraScript cs;
+    //odleglosc spawnowania w pionie i poziomie od srodka
+    float verticalDistance, horizontalDistance;
+    //licznik do nastepnego spawanu
+    float spawnTimer;
+    //co ile ma byc spawn
+    public float spawnInterval = 5;
+    //prefab kamulec
+    public GameObject kamulecPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cs = Camera.main.GetComponent<CameraScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer > spawnInterval)
+        {
+            //spawnujemy kamien
+            Vector3 spawnPosition = getRandomSpawnPosition();
+            GameObject asteroid = Instantiate(kamulecPrefab, spawnPosition, Quaternion.identity);
+            //liczymy wektor w kierunku gracza
+            //resetujemy timer
+            spawnTimer = 0;
+        }
+    }
+    Vector3 getRandomSpawnPosition()
+    {
+        //policz odlegosc spawnowania
+        verticalDistance = 0.55f * cs.gameHeight;
+        horizontalDistance = 0.55f * cs.gameWidth;
+        //losowanie liczby miedzy <1;4>
+        int randomSpawnLine = Random.Range(1, 5);
+        Vector3 randomSpawnLocation = Vector3.zero;
+        switch (randomSpawnLine)
+        {
+            case 1:
+                //gorna linia
+                randomSpawnLocation = new Vector3(Random.Range(-horizontalDistance, horizontalDistance),
+                                                                0,
+                                                                verticalDistance);
+                break;
+            case 2:
+                //prawa linia
+                randomSpawnLocation = new Vector3(horizontalDistance,
+                                                    0,
+                                                    Random.Range(-verticalDistance, verticalDistance));
+                break;
+            case 3:
+                //dolna linia
+                randomSpawnLocation = new Vector3(Random.Range(-horizontalDistance, horizontalDistance),
+                                                                0,
+                                                                -verticalDistance);
+                break;
+            case 4:
+                //lewa linia
+                randomSpawnLocation = new Vector3(-horizontalDistance,
+                                                    0,
+                                                    Random.Range(-verticalDistance, verticalDistance));
+                break;
+        }
+        return randomSpawnLocation;
+    }
+}
